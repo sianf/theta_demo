077cdcc8085ae8bb055964da4245459a53459da1
Nakajima Kazuki <nakajima.k0301@gmail.com>
2020-02-05 18:04:37 +0900

    update

--- js/psv.js
+++ js/psv.js
@@ -1,16 +1,20 @@
-let viewer = new PhotoSphereViewer({
+let PSV = new PhotoSphereViewer({
   container: 'viewer',
-  panorama: '/img/' + getImageFileName()
+  panorama: '/img/' + getBasename() + String(1) + '.jpg',
+  navbar: 'autorotate zoom download fullscreen',
+  time_anim: false
 });
 
+function getBasename() {
+  let p = window.location.href.split('/').pop();
+  return p.split('/').pop().split('.').shift();
+}
+
 /**
- * 各HTMLファイルに対応する画像ファイル名を取得して返す
- * /img/ディレクトリには、HTML名と1対1対応する画像を配置する。
- * 例えば、/pages/dog.html <-> /img/dog.jpgなど
- * 
- * ※ 現状は.jpgのみ対応
+ * PSVで描画するファイルを選択する
+ * @param {int} image_index 
  */
-function getImageFileName() {
-  var current_html_file_name = window.location.href.split('/').pop();
-  return current_html_file_name.replace('html', 'jpg')
+function selectPsvMedia(image_index) {
+  let p = '/img/' + getBasename() + String(image_index) + '.jpg'
+  PSV.setPanorama(p)
 }

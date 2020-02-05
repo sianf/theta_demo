aaa22f3b5b52fb427fc92f06d3f4d547cfffe696
Nakajima Kazuki <nakajima.k0301@gmail.com>
2020-02-05 11:42:58 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # theta_demo
 
 Thetaで撮影した360°画像を表示するデモ
+
+## ページ追加
+
+パノラマ画像`city.jpg`を表示させる場合
+
+1. pages/sample.htmlをコピーして名前をcity.htmlに変更
+1. imgディレクトリにcity.jpgを追加
+1. index.htmlにリンクを追加
--- js/psv.js
+++ js/psv.js
@@ -1,4 +1,16 @@
-var viewer = new PhotoSphereViewer({
+let viewer = new PhotoSphereViewer({
   container: 'viewer',
-  panorama: 'img/panorama.jpg'
+  panorama: '/img/' + getImageFileName()
 });
+
+/**
+ * 各HTMLファイルに対応する画像ファイル名を取得して返す
+ * /img/ディレクトリには、HTML名と1対1対応する画像を配置する。
+ * 例えば、/pages/dog.html <-> /img/dog.jpgなど
+ * 
+ * ※ 現状は.jpgのみ対応
+ */
+function getImageFileName() {
+  var current_html_file_name = window.location.href.split('/').pop();
+  return current_html_file_name.replace('html', 'jpg')
+}
